9cb47347193338d0dc1557312778c4d0416692da
sonsumin <sonsumin00@gmail.com>
2024-10-18 15:03:53 +0900

    first commit

---
+++ src/test/java/com/example/myDiary/MyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
