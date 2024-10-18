6f30cb37d7070e5f1cf1c64971f8417d1ce9c6ea
sonsumin <sonsumin00@gmail.com>
2024-10-18 16:10:51 +0900

    [#1] Feat: 멤버 컨트롤러

---
+++ src/main/java/com/diary/myDiary/domain/member/controller/MemberController.java
@@ -0,0 +1,74 @@
+package com.diary.myDiary.domain.member.controller;
+
+
+import com.diary.myDiary.domain.member.dto.*;
+import com.diary.myDiary.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원가입
+     */
+    @PostMapping("/signUp")
+    @ResponseStatus(HttpStatus.OK)
+    public void singUp(@Valid @RequestBody MemberSignUpDTO memberSignUpDTO) throws Exception {
+        memberService.signup(memberSignUpDTO);
+    }
+
+    /**
+     * 회원정보수정
+     */
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void updateInfo(@Valid @RequestBody MemberUpdateDTO memberUpdateDTO) throws Exception {
+        memberService.update(memberUpdateDTO);
+    }
+
+    /**
+     * 비밀번호수정
+     */
+    @PutMapping("/password")
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePassword(@Valid @RequestBody UpdatePasswordDTO updatePasswordDTO) throws Exception {
+        memberService.updatePassword(updatePasswordDTO.checkPassword(), updatePasswordDTO.newPassword());
+    }
+
+    /**
+     * 회원탈퇴
+     */
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void withdraw(@Valid @RequestBody MemberWithdrawDTO memberWithdrawDTO)throws Exception {
+        memberService.withdraw(memberWithdrawDTO.checkPassword());
+    }
+
+    /**
+     * 회원정보조회
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity getInfo(@Valid @PathVariable("id") Long id) throws Exception {
+        MemberInfoDTO info = memberService.getInfo(id);
+        return new ResponseEntity(info, HttpStatus.OK);
+    }
+
+    /**
+     * 내정보조회
+     */
+    @GetMapping
+    public ResponseEntity getMyInfo(HttpServletResponse response) throws Exception {
+
+        MemberInfoDTO myInfo = memberService.getMyInfo();
+        return new ResponseEntity(myInfo, HttpStatus.OK);
+    }
+}
