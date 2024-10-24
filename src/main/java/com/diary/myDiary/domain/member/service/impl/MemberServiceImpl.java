82af9d69678cf85f755dafdf32081c912b82bb49
khyu2 <ikeelee7742@gmail.com>
2024-10-25 03:41:46 +0900

    [#1] Fix: MemberServiceImpl 단에 Transactional 이 걸려있지 않는 문제 수정

--- src/main/java/com/diary/myDiary/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/diary/myDiary/domain/member/service/impl/MemberServiceImpl.java
@@ -13,10 +13,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
